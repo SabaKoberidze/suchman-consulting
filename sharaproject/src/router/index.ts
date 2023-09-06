c758bab1830341080d91cbdceae82171c2295b6c
SabaKoberidze <koberidzesaba@gmail.com>
2023-09-06 05:23:56 +0400

    fixed

--- sharaproject/src/router/index.ts
+++ sharaproject/src/router/index.ts
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   linkActiveClass: "active-link",
   linkExactActiveClass: "exact-active-link",
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior() {
     // always scroll to top
     return { top: 0, behavior: 'smooth' }
   },
